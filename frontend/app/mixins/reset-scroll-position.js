7cae6630c72c30a7c27725e6a8f1b88e139fc7e3
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-20 18:33:42 +0200

    fix reset scroll position mixin [ci skip]

--- frontend/app/mixins/reset-scroll-position.js
+++ frontend/app/mixins/reset-scroll-position.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  fastboot: Ember.inject.service(),
   activate: function() {
     this._super();
-    window.scrollTo(0,0);
+    if (!this.get('fastboot.isFastBoot')) {
+      window.scrollTo(0,0);
+    }
   }
 });
